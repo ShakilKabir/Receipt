15a79598a54dfbda92da9970f916b9b7e65c834c
ShakilKabir <shaakilkabir@gmail.com>
2024-08-31 12:28:54 +0600

    completed without product reference

--- src/pages/ReceiptPage.jsx
+++ src/pages/ReceiptPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import Receipt from "./Receipt";
+import Receipt from "./receipt";
 
 const ReceiptPage = () => {
   const location = useLocation();
